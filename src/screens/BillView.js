f87b7aa47dff83db2ed92fb977dcafbbfb5c75db
Dixit Gogoi <dixitgogoi2017@gmail.com>
2024-05-25 14:22:59 +0530

    table updated

--- src/screens/BillView.js
+++ src/screens/BillView.js
@@ -115,56 +115,60 @@ const BillView = ({ route }) => {
             <p style="margin: 0; font-weight: 500; font-size: 12px; margin-bottom: 1px; "><u>Colour: ${item.color}</u></p>
             <u style="margin: 0; font-weight: 500; font-size: 12px;">${item.type}</u>
           </td>
-          <td style="font-size: 10px; border: 0.5px solid black; width: 8%; padding: 10px; ">
-            <p style="margin: 0; font-weight: 500; font-size: 11px;">${item.thickness}</p>
+          <td style="font-size: 10px; border: 0.5px solid black; width: 8%; padding: 3px; ">
+            <p style="margin: 0; font-weight: 500; font-size: 12px;">${item.thickness}</p>
           </td>
-          <td style="font-size: 10px; border: 0.5px solid black; width: 8%; padding: 10px; ">
+          <td style="font-size: 10px; border: 0.5px solid black; width: 8%; padding: 6px; ">
             <p style="margin: 0; font-weight: 500; font-size: 12px;">${item.width}</p>
           </td>
+
           <td style="font-size: 10px; border: 0.5px solid black; width: 8%; vertical-align: top; padding: 0;">
-            <div style="border-bottom: 0.5px solid black; height: 15px; display: flex; align-items: center; justify-content: center; padding: 10px;">
-              <p style="margin: 0; font-weight: 500; font-size: 9px;">${item.length} ${item.unit}</p>
+            <div style="border-bottom: 0.5px solid black; height: 35px; display: flex; align-items: center; justify-content: center; padding: 0;">
+              <p style="margin: 0; font-weight: 500; font-size: 11px;">${item.length} ${item.unit}</p>
             </div>
             <div style="height: 1px; background-color: black;"></div>
-            <div style="height: 15px; display: flex; align-items: center; justify-content: center; padding: 10px;">
+            <div style="height: 35px; display: flex; align-items: center; justify-content: center; padding: 0;">
               <p style="margin: 0; font-weight: 500;">Total</p>
             </div>
           </td>
-          <td style="font-size: 10px; border: 0.5px solid black; width: 8%; padding: 10px; vertical-align: top; padding: 0;">
-            <div style="border-bottom: 0.5px solid black; height: 15px; padding: 10px; display: flex; align-items: center; justify-content: center;">
+
+          <td style="font-size: 10px; border: 0.5px solid black; width: 8%; padding: 0; vertical-align: top; ">
+            <div style="border-bottom: 0.5px solid black; height: 35px; padding: 0; display: flex; align-items: center; justify-content: center;">
               <p style="margin: 0; font-weight: 500;">${item.pieces}</p>
             </div>
             <div style="height: 1px; background-color: black;"></div>
-            <div style="height: 15px; display: flex; align-items: center; padding: 10px; justify-content: center;">
+            <div style="height: 35px; display: flex; align-items: center; padding: 0; justify-content: center;">
               <p style="margin: 0; font-weight: 500;">${item.pieces}</p>
             </div>
           </td>
-          <td style="font-size: 10px; border: 0.5px solid black; width: 15%; padding: 0px; vertical-align: top;">
-            <div style="border-bottom: 0.5px solid black; height: 15px; display: flex; align-items: center; justify-content: center; padding: 10px;">
+
+          <td style="font-size: 10px; border: 0.5px solid black; width: 15%; padding: 0; vertical-align: top;">
+            <div style="border-bottom: 0.5px solid black; height: 35px; display: flex; align-items: center; justify-content: center; padding: 0;">
               <p style="margin: 0; font-weight: 500;">${item.pieces * item.length}</p>
             </div>
             <div style="height: 1px; background-color: black;"></div>
-            <div style="display: flex; height: 30px; ">
-              <div style="width: 65%; display: flex; align-items: center; justify-content: center; padding: 10px;">
+            <div style="display: flex; height: 35px; ">
+              <div style="width: 65%; display: flex; align-items: center; justify-content: center; padding: 0;">
                 <p style="margin: 0; font-weight: 500;">${item.pieces * item.length}</p>
               </div>
               <div style="width: 1px; background-color: black; height: 100%;"></div>
-              <div style="width: 35%; display: flex; align-items: center; justify-content: center; padding: 10px;">
+              <div style="width: 35%; display: flex; align-items: center; justify-content: center; padding: 0;">
                 <p style="margin: 0; font-weight: 600;">Rft</p>
               </div>
             </div>
           </td>
-          <td style="font-size: 10px; border: 0.5px solid black; width: 17%; padding: 0px; vertical-align: top;">
-            <div style="border-bottom: 0.5px solid black; height: 15px; padding: 10px; display: flex; align-items: center; justify-content: center;">
+
+          <td style="font-size: 10px; border: 0.5px solid black; width: 17%; padding: 0; vertical-align: top;">
+            <div style="border-bottom: 0.5px solid black; height: 35px; padding: 0; display: flex; align-items: center; justify-content: center;">
               <p style="margin: 0; font-weight: 600;">Per Rft</p>
             </div>
             <div style="height: 1px; background-color: black;"></div>
-            <div style="display: flex; height: 30px; padding: 0px; ">
-              <div style="width: 20%; display: flex; align-items: center; justify-content: center; padding: 10px;">
+            <div style="display: flex; height: 35px; ">
+              <div style="width: 20%; display: flex; align-items: center; justify-content: center; padding: 0;">
                 <p style="margin: 0; font-weight: 500;">₹</p>
               </div>
               <div style="width: 1px; background-color: black;"></div>
-              <div style="width: 80%; display: flex; align-items: center; justify-content: center; padding: 10px;">
+              <div style="width: 80%; display: flex; align-items: center; justify-content: center; padding: 0;">
                 <p style="margin: 0; font-weight: 600;">${item.rate}.00</p>
               </div>
             </div>
@@ -417,7 +421,7 @@ const BillView = ({ route }) => {
     <p>4. Rates are subject to change without any Prior Information.</p>
     <div>
       <p>5. FOR BANK DETAILS:- Name: Pooja Roofing Co. (MFG) A/C NO: 41122724588 (STATE BANK OF INDIA,)</p>
-      <p>IFSC CODE: SBIN0013246 (Traders Branch Fancy Bazar)</p>
+      <p style="padding-left: 5px;">IFSC CODE: SBIN0013246 (Traders Branch Fancy Bazar)</p>
     </div>
     <p>6. Transportation: Client's Own Arrangement / To Pay Basis</p>
     <p>7. The above Rates are valid for 7 Days</p>
