86e4354b91bada8d0ba2adc37634648275c55dfb
Dixit Gogoi <dixitgogoi2017@gmail.com>
2024-07-20 18:24:27 +0530

    updated

--- src/screens/EditOrder.js
+++ src/screens/EditOrder.js
@@ -1,6 +1,6 @@
 import { useNavigation } from '@react-navigation/native';
-import { StyleSheet, Text, View, SafeAreaView, StatusBar, TouchableOpacity } from 'react-native'
-import { zomatoRed } from '../utils/colors';
+import { StyleSheet, Text, View, SafeAreaView, StatusBar, TouchableOpacity, ScrollView } from 'react-native'
+import { lightZomatoRed, modalBackColor, zomatoRed } from '../utils/colors';
 import { responsiveFontSize } from 'react-native-responsive-dimensions';
 import Icon from 'react-native-vector-icons/dist/MaterialIcons';
 
@@ -31,12 +31,38 @@ const EditOrder = (route) => {
                 </View>
             </View>
 
-            {details.orderDetails.map(item => (
-                <View style={{padding: 10, borderColor: zomatoRed, borderWidth: 1}}>
-                    <Text style={{ color: '#000' }}>{item.product_type}</Text>
-                    <Text style={{ color: '#000' }}>{item.color}</Text>
+            <ScrollView style={{ flex: 1 }}>
+                <View style={{ padding: 10 }}>
+                    {details.orderDetails.map(item => (
+                        <View style={{ backgroundColor: '#fff', borderRadius: 8, width: '100%', paddingHorizontal: 11, flexDirection: 'column', gap: 8, paddingVertical: 12, elevation: 1, marginBottom: 8 }} key={item?.id}>
+
+                            {/* Header */}
+                            <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', backgroundColor: lightZomatoRed, padding: 5, borderRadius: 8, borderColor: zomatoRed, borderWidth: 0.6 }}>
+                                <Text style={{ color: zomatoRed, fontWeight: '600', fontSize: responsiveFontSize(2.3), textTransform: 'uppercase', marginLeft: 5 }}>{item?.product_type}</Text>
+                                <Text style={{ color: zomatoRed, fontWeight: '600', fontSize: responsiveFontSize(2.3), textTransform: 'uppercase', marginLeft: 5 }}>{item?.color}</Text>
+                            </View>
+
+                            {/* Second Section */}
+                            <View style={{ flexDirection: 'column', gap: 5, backgroundColor: modalBackColor, paddingVertical: 8, paddingHorizontal: 12, borderRadius: 10, elevation: 1, justifyContent: 'space-between' }}>
+                                {item.orderData.map(it => (
+                                    <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', width: '100%', }} key={it?.id}>
+                                        <Text style={{ color: '#585858', fontSize: responsiveFontSize(2.2), fontWeight: '500' }}>Pieces: </Text>
+                                        {/* <View style={{ flexDirection: 'row', alignItems: 'center', gap: 5, width: '50%', }}>
+                                            <Text style={{ color: '#585858', fontSize: responsiveFontSize(2.2), fontWeight: '500' }}>Length:</Text>
+                                            <Text style={{ color: '#000', fontSize: responsiveFontSize(2.2), fontWeight: '500' }}>{it?.length} {item.unit}</Text>
+                                        </View> */}
+                                        {/* <View style={{ flexDirection: 'row', alignItems: 'center', gap: 5, width: '50%', justifyContent: 'flex-end' }}>
+                                            <Text style={{ color: '#585858', fontSize: responsiveFontSize(2.2), fontWeight: '500' }}>Pieces:</Text>
+                                            <Text style={{ color: '#000', fontSize: responsiveFontSize(2.2), fontWeight: '500' }}>{it?.quantity}</Text>
+                                        </View> */}
+                                    </View>
+                                ))}
+                            </View>
+
+                        </View>
+                    ))}
                 </View>
-            ))}
+            </ScrollView>
 
         </SafeAreaView>
     )
